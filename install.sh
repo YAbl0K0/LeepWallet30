ba434004c465f09ed437cc59f6dd4ec0e0a9c6af
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-08-28 19:30:50 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-https://github.com/YAbl0K0/LeepWallet30/blob/main/crate.git
+https://github.com/YAbl0K0/LeepWallet30.git
 cd repository
 npm install
-node generateLeapWallets.js
+node crate.js
 
 chmod +x run.sh
 ./run.sh
