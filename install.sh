935a971138e9f87e711fb697d3f58a598358dbb8
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-08-28 19:35:05 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
 https://github.com/YAbl0K0/LeepWallet30.git
 cd LeepWallet30
 npm install
 node crate.js
 
-chmod +x run.sh
-./run.sh
+if [ -f run.sh ]; then
+  chmod +x run.sh
+  ./run.sh
+fi
