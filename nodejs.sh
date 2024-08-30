34974b3741d8fd555328e6ec6ef2c4c0fd8b2af3
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-08-30 15:49:37 +0300

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+
 sudo apt update
 sudo apt install -y nodejs npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 source ~/.bashrc
+
 nvm install 16
 nvm use 16
 
